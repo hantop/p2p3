14d868414fec44e73bec20508964d7164d752d5b
Boyley <274163579@qq.com>
2015-07-22 15:29:46 +0800

    common

--- modules/spring-security/src/main/java/com/fenlibao/p2p/security/config/SecurityConfig.java
+++ modules/spring-security/src/main/java/com/fenlibao/p2p/security/config/SecurityConfig.java
@@ -1,15 +1,30 @@
 package com.fenlibao.p2p.security.config;
 
 import com.fenlibao.p2p.security.service.impl.UserDetailsService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.access.AccessDecisionManager;
+import org.springframework.security.access.AccessDecisionVoter;
+import org.springframework.security.access.hierarchicalroles.RoleHierarchy;
+import org.springframework.security.access.hierarchicalroles.RoleHierarchyImpl;
+import org.springframework.security.access.vote.AffirmativeBased;
+import org.springframework.security.access.vote.RoleHierarchyVoter;
+import org.springframework.security.access.vote.RoleVoter;
+import org.springframework.security.access.vote.UnanimousBased;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.configurers.UrlAuthorizationConfigurer;
+import org.springframework.security.web.FilterInvocation;
+import org.springframework.security.web.access.expression.WebExpressionVoter;
 import org.springframework.security.web.util.AntPathRequestMatcher;
 
 import javax.inject.Inject;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by Administrator on 2015/6/19.
@@ -18,37 +33,90 @@ import javax.inject.Inject;
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Inject
-    private ApplicationContext context;
-
-    @Inject
     private UserDetailsService userDetailsService;
 
     @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests()
-                .antMatchers("/login", "/register").permitAll()
-                .antMatchers("/**").hasRole("USER")
-                .anyRequest()
-                .fullyAuthenticated().and()
-                .formLogin().loginPage("/login").usernameParameter("username").passwordParameter("password").loginProcessingUrl("/login_check")
-                .failureUrl("/login?error").and()
-                .logout().logoutRequestMatcher(new AntPathRequestMatcher("/logout"));
+    public void init(WebSecurity web) throws Exception {
+        web.debug(true);
+        super.init(web);
+
     }
+//
+//    @Override
+//    protected void configure(HttpSecurity http) throws Exception {
+//        http.authorizeRequests()
+//                .antMatchers("/login", "/register").permitAll()
+//                .antMatchers("/**").hasRole("ADMIN")
+//                .anyRequest()
+//                .fullyAuthenticated().and()
+//                .formLogin().loginPage("/login").usernameParameter("username").passwordParameter("password").loginProcessingUrl("/login_check")
+//                .failureUrl("/login?error").and()
+//                .logout().logoutRequestMatcher(new AntPathRequestMatcher("/logout"));
+//    }
+
 
     @Override
     public void configure(WebSecurity web) throws Exception {
         web
                 .ignoring()
-                .antMatchers("/resources/**", "/weixin/**", "/zhaopin/**","/sign/**"); // 忽略任何以”/resources/”开头的请求，这和在XML配置http@security=none的效果一样
-    }
-
-    @Inject
-    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
-        auth.userDetailsService(userDetailsService);
+                .antMatchers("/resources/**", "/weixin/**", "/zhaopin/**", "/sign/**"); // 忽略任何以”/resources/”开头的请求，这和在XML配置http@security=none的效果一样
     }
 
     @Override
-    protected UserDetailsService userDetailsService() {
-        return userDetailsService;
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userDetailsService);
     }
+
+
+    @Bean
+    public RoleVoter roleVoter() {
+        return new RoleHierarchyVoter(roleHierarchy());
+    }
+
+    @Bean
+    public RoleHierarchy roleHierarchy() {
+        RoleHierarchyImpl roleHierarchy = new RoleHierarchyImpl();
+        roleHierarchy.setHierarchy("ADMIN > PREMIUM  PREMIUM > USER  USER > GUEST");
+        return roleHierarchy;
+    }
+
+    @Bean
+    public AffirmativeBased accessDecisionManager() {
+        List<AccessDecisionVoter> decisionVoters = new ArrayList<>();
+        WebExpressionVoter webExpressionVoter = new WebExpressionVoter();
+        decisionVoters.add(webExpressionVoter);
+        decisionVoters.add(roleVoter());
+        AffirmativeBased affirmativeBased = new AffirmativeBased(decisionVoters);
+        affirmativeBased.setAllowIfAllAbstainDecisions(true);
+        return affirmativeBased;
+    }
+    //
+//    @Bean
+//    public RoleHierarchyVoter roleVoter() {
+//        RoleHierarchyImpl roleHierarchy = new RoleHierarchyImpl();
+//        roleHierarchy.setHierarchy("ROLE_ADMIN > ROLE_USER > ROLE_ANONYMOUS");
+//        RoleHierarchyVoter roleHierarchyVoter = new RoleHierarchyVoter(roleHierarchy);
+//        return roleHierarchyVoter;
+//    }
+//
+//
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .formLogin().loginPage("/login").usernameParameter("username").passwordParameter("password").loginProcessingUrl("/login_check")
+                .failureUrl("/login?error").and()
+                .logout().logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
+                .deleteCookies("JSESSIONID")
+                .logoutSuccessUrl("/").and()
+                .authorizeRequests()
+                .accessDecisionManager(accessDecisionManager())
+                .antMatchers("/login", "/register").permitAll()
+                .antMatchers("/**").hasRole("USER")
+                .antMatchers("/login", "/signup/email", "/logout", "/search", "/").permitAll()
+                .anyRequest().authenticated();
+
+    }
+
+
+
 }
