2c6332bfd64be55f4406b5cb205d51568c93022c
Boyley <274163579@qq.com>
2015-07-23 12:05:08 +0800

    spring security

--- modules/spring-security/src/test/java/com/fenlibao/SecurityTest.java
+++ modules/spring-security/src/test/java/com/fenlibao/SecurityTest.java
@@ -2,6 +2,7 @@ package com.fenlibao;
 
 import org.springframework.security.config.annotation.web.configurers.LogoutConfigurer;
 import org.springframework.security.web.FilterChainProxy;
+import org.springframework.security.web.access.expression.DefaultWebSecurityExpressionHandler;
 import org.springframework.security.web.access.intercept.FilterSecurityInterceptor;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.security.web.authentication.logout.LogoutFilter;
@@ -20,4 +21,5 @@ public class SecurityTest {
     LogoutFilter logoutFilter;
     LogoutConfigurer configurer;
     UsernamePasswordAuthenticationFilter usernamePasswordAuthenticationFilter;
+    DefaultWebSecurityExpressionHandler defaultWebSecurityExpressionHandler;
 }
